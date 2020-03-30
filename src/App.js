f3037561655ede3b05b244a87331518edffb5989
Cassandra Rossall <cassandrarossall@Cassandras-MacBook-Pro.local>
2020-03-30 14:34:11 +1300

    receiving status 429, waiting to test handleSubmit and handleInput functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+
 import './App.scss'
+import {API_KEY} from './base'
 
 const App = () => {
   const [data, setData] = useState([])
-  const [article, setArticle] = useState([])
+  const [query, setQuery] = useState('donald-trump')
   const [isLoading, setIsLoading] = useState(false)
   const [articleNum, setArticleNum] = useState(0)
+  const [value, setValue] = useState('')
   const [url, setUrl] = useState(
-    'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=4792a2b1fc8446f094c1d461b6330502')
+    `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`)
 
   useEffect(() => {
     setIsLoading(true)
@@ -19,24 +22,50 @@ const App = () => {
     }).catch(error => console.log(error))
   }, [articleNum])
 
-  const handleClick = () => {
-    if(articleNum == 9) {
-      setArticleNum(0)
-    }
-    setArticleNum(prevNum => prevNum + 1)
+  const handleClick = type => {
+    if(type === 'prev') setArticleNum(prevNum => prevNum - 1);
+    if(type === 'next') setArticleNum(prevNum => prevNum + 1)
+    if(articleNum === 9 && type === 'next') setArticleNum(0)
+    if(articleNum === 0 && type === 'prev') setArticleNum(9)
+  }
+
+  const handleSubmit = event => {
+      event.preventDefault()
+      setQuery(value);
+      // event.target.reset()
+  }
+
+  const handleInput = event => {
+    setValue(event.target.value)
   }
   
   return (
-    <div className="App">
+    <div className="container">
+      <div className="search-input">
+        <form onSubmit = {handleSubmit}>
+          <input type="text" onChange = {handleInput} value={value} placeholder= '&#xF002; set(query)'/>
+        </form>
+      </div>
         <div className = 'article'>
           <h1>{data.title}</h1>
-          <span>{data.publishedAt}</span>
-          <img src={data.urlToImage} alt=""/>
-          <h2>{data.description}</h2>
-          <a href= {data.url} target='_blank' rel="noopener noreferrer">Read Story</a>
-        </div>
+            <img src={data.urlToImage} alt=""/>
+            <span>{data.publishedAt}</span>
+            <p>{data.description}</p>
+            <a href= {data.url} target='_blank' rel="noopener noreferrer">Read Story</a>
 
-        <button onClick = {handleClick}>Next article</button>
+            <div className="buttons">
+              <button 
+                className = 'prev-btn' 
+                onClick = {() => {handleClick('prev')}}>
+                Previous article
+              </button>
+              <button 
+                className = 'next-btn' 
+                onClick = {() => {handleClick('next')}}>
+                Next article
+              </button>
+            </div>
+        </div>
     </div>
   )}
 
